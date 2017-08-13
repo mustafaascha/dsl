ec6bb98571620ec992053f388216c2bd3a0a638c
Thomas Mailund <mailund@birc.au.dk>
2017-08-13 13:55:34 +0200

    lcomp implementation

---
+++ R/lcomp/lcomp.R
@@ -0,0 +1,39 @@
+library(rlang)
+
+lcomp <- function(expr, ...) {
+  expr <- enquo(expr)
+  rest <- quos(...)
+  
+  lists <- Map(eval_tidy, rest[names(rest) != ""])
+  predicates <- Map(UQE, rest[names(rest) == ""])
+  
+  f <- new_function(lists, body = UQE(expr), env = get_env(expr))
+  values <- do.call(Map, c(f, lists))
+  
+  keep_index <- rep(TRUE, length(lists[[1]]))
+  for (pred in predicates) {
+    p <- new_function(lists, body = pred, env = get_env(expr))
+    keep_index <- keep_index & unlist(do.call(Map, c(p, lists)))
+  }
+  
+  values[keep_index]
+}
+
+
+qsort <- function(x) {
+  n <- length(x)
+  
+  if (n < 2) return(x)
+  
+  pivot <- x[[sample(n, size = 1)]]
+  smaller <- lcomp(y, y = x, y < pivot)
+  equal <- lcomp(y, y = x, y == pivot)
+  larger <- lcomp(y, y = x, y > pivot)
+  
+  c(qsort(smaller), equal, qsort(larger))
+}
+
+(x <- sample(1:10))
+unlist(qsort(x))
+
+
