be8f6743199484c737572384bfc53ed843e51acb
Thomas Mailund <mailund@birc.au.dk>
2018-01-30 14:26:42 +0100

    Pattern matching

--- R/PatternMatch/match.R
+++ R/PatternMatch/match.R
@@ -1,13 +1,96 @@
 
-constructor <- function() NA
-variable <- function() NA
+test_pattern_rec <- function(escape, expr, test_expr, eval_env, match_env) {
+  constructor <- NULL
+  if (is_lang(test_expr)) {
+    func <- get(as_string(test_expr[[1]]), eval_env)
+    if ("constructor" %in% class(func))
+      constructor <- as_string(test_expr[[1]])
+  }
 
-T <- L <- constructor()
-v <- w <- variable()
+  if (is_null(constructor)) {
+    # Not a constructor.
+    # Must be a value to compare with or a variable to bind to
+    if (is_symbol(test_expr)) {
+      assign(as_string(test_expr), expr, match_env)
+    } else {
+      value <- eval_tidy(test_expr, eval_env)
+      if (expr != value) escape(NULL)
+    }
 
-match <- function(expr, ...) {
+  } else {
+    # This is a constructor.
+    # Check if it is the right kind
+    expr_constructor <- attr(expr, "constructor")
+    if (is_null(expr) || constructor != expr_constructor)
+      escape(NULL) # wrong type
 
+    for (i in seq_along(expr)) {
+      test_pattern_rec(escape, expr[[i]], test_expr[[i+1]], eval_env, match_env)
+    }
+  }
+
+  match_env
 }
 
-match(T(L(4), L(5)),
-      T(L(v), L(w)) := v + w)
+test_pattern <- function(expr, test_expr, eval_env) {
+  # Environment in which to store matched variables
+  match_env <- env()
+
+  if (test_expr == quote(otherwise))
+    return(match_env)
+
+  # Test pattern
+  tester <- function(escape)
+    test_pattern_rec(escape, expr, test_expr, eval_env, match_env)
+  callCC(tester)
+}
+
+match <- function(expr, ...) {
+  matchings <- quos(...)
+  matchings[[1]]
+
+  for (i in seq_along(matchings)) {
+    eval_env <- get_env(matchings[[i]])
+    match_expr <- quo_expr(matchings[[i]])
+    stopifnot(match_expr[[1]] == "<-")
+
+    test_expr <- match_expr[[3]]
+    result_expr <- match_expr[[2]]
+
+    match <- test_pattern(expr, test_expr, eval_env)
+    if (!is_null(match))
+      return(eval_tidy(result_expr, data = match, env = eval_env))
+  }
+
+  stop("No matching pattern!")
+}
+
+
+tree := T(left : tree, right : tree) | L(value : numeric)
+
+match(L(1),
+      L(2) -> 12,
+      L(1) -> 11)
+
+
+matching <- function(expr)
+  match(expr,
+        L(v) -> v,
+        T(L(v), L(w)) -> v + w,
+        otherwise -> 5)
+
+matching(L(1))
+matching(T(L(4), L(5)))
+matching(T(L(1), T(L(4), L(5))))
+
+dfs <- function(tree) {
+  match(tree,
+        L(v) -> v,
+        T(left, right) -> dfs(left) + dfs(right))
+}
+
+dfs(L(1))
+dfs(T(L(1),L(2)))
+
+x <- T(T(L(1),L(2)),L(3))
+dfs(x)
